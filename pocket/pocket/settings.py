9572bee01b3bf6d80ef72ab9ffb064e558b416a2
Kendryck-Garcia <garciakendryck@gmail.com>
2026-09-14 00:24:41 -0400

    Configure ALLOWED_HOSTS and STATIC_ROOT for PythonAnywhere cloud deployment

--- pocket/pocket/settings.py
+++ pocket/pocket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-usq8(gyn7=@ec(qldqr&ps%_6(w1v%bkb#x6j6p3l)9l%9&v89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
